e6db3851cbf50078cec605eb3591b8878e07844f
martin-labsky <martin.labsky@cz.ibm.com>
2016-12-11 22:22:29 +0100

    feat(app): Add Conversation UI shown in an iframe
    Signed-off-by: martin-labsky <martin.labsky@cz.ibm.com>

--- public/js/api.js
+++ public/js/api.js
@@ -6,6 +6,8 @@ var Api = (function() {
   var curWorkspace;
   var messageEndpoint = '/api/message';
 
+  var rrEndpoint = 'https://gateway.watsonplatform.net/retrieve-and-rank/api/v1/solr_clusters/sc37b37ace_752a_4180_a581_a8c2d2189087/solr/coco_collection/select?q=expression&wt=json&fl=author';
+
   // Publicly accessible methods defined
   return {
     sendRequest: sendRequest,
@@ -22,7 +24,21 @@ var Api = (function() {
       return responsePayload;
     },
     setResponsePayload: function(newPayloadStr) {
-      responsePayload = JSON.parse(newPayloadStr);
+      log("response: "+newPayloadStr);
+      try {
+	      responsePayload = JSON.parse(newPayloadStr);
+	      if(responsePayload.output.url) {
+	    	  document.getElementById("doc").src = responsePayload.output.url;
+	    	  //var iframe = document.getElementById("doc").src = "https://www.google.com/webhp?ie=UTF-8#q=intent%20site%3Ahttp%3A%2F%2Fwww.ibm.com%2Fwatson%2Fdevelopercloud%2Fdoc%2Fconversation";
+	      }
+	      else if(responsePayload.output.rnr) {
+	          log("Querying R&R: "+newPayloadStr);
+	    	  sendRRRequest(responsePayload.output.rnr);
+	      }
+      }
+      catch(e) {
+    	  log("error processing conversation repsonse: "+e);
+      }
     },
     getWorkspace: function() {
         return curWorkspace;
@@ -30,10 +46,32 @@ var Api = (function() {
     setWorkspace: function(wksp) {
         curWorkspace = wksp;
         //alert(curWorkspace);
-    }
+    },
 
+    displayRRResult: function(rrResponse) {
+      log("RR response: "+rrResponse);
+      var p = JSON.parse(rrResponse);
+      var handled = false;
+      try {
+	      if(p && p.response && p.response.docs && p.response.docs.length > 0) {
+	      	  document.getElementById("doc").src = p.response.docs[0].author[0];
+	      	  handled = true;
+	      }
+      }
+	  catch (e) {
+		log("No luck reading R&R response: "+p);
+	  }
+      if (!handled) {
+    	  // TODO: add msg to chatbox 
+      }
+    }
+    
   };
 
+  function log(msg) {
+	  var el = document.getElementById("select_wksp").innerText += "\n" + msg;
+  }
+  
   // Send a message request to the server
   function sendRequest(text, context) {
     // Build request payload
@@ -68,7 +106,27 @@ var Api = (function() {
       Api.setRequestPayload(params);
     }
 
+	log("req:"+params+"\n");
+    
     // Send request
     http.send(params);
   }
+  
+  // Send a message request direct to the R&R server
+  function sendRRRequest(text) {
+    // Built http request
+    var http = new XMLHttpRequest();
+    http.open('GET', rrEndpoint, true);
+    //alert("endpoint:"+messageEndpoint);
+    http.setRequestHeader('Authorization', secret);
+    http.onreadystatechange = function() {
+      log("RR state: "+http.readyState+" status: "+http.status +" resp: "+http.responseText);
+      if (http.readyState === 4 && http.status === 200 && http.responseText) {
+        Api.displayRRResult(http.responseText);
+      }
+    };
+    // Send request
+    http.send();
+  }
+
 }());
